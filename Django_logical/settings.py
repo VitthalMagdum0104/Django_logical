b0b838f109d728363126b4866d7b835ad6ea88a0
VitthalMagdum0104 <magdumvitthal01@gmail.com>
2021-08-28 16:55:14 +0530

    Update settings.py

--- Django_logical/settings.py
+++ Django_logical/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7bwvn0%k$h=94%m@tz*(x)7a_3g^85zhzps!44y$#n9v_)!hjc'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
